f93a95cb3eb5c5be212f2d3b2c7453b0cb488ec4
Yuriy <Seaman_on_watch@mail.ru>
2020-05-04 15:07:05 +0300

    Saving subject of the command

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -11,43 +11,86 @@ describe('Our first suite', () => {
 
     it('first test', () => {
 
-            cy.visit('/')
-            cy.contains('Forms').click()
-            cy.contains('Form Layouts').click()
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
 
+        // by tag name
+        cy.get('input')
 
-            // by tag name
-            cy.get('input')
+        // by id
+        cy.get('#inputEmail1')
 
-            // by id
-            cy.get('#inputEmail1')
+        // by class name
+        cy.get('.input-full-width')
 
-            // by class name
-            cy.get('.input-full-width')
+        // by attribute name
+        cy.get('[placeholder]')
 
-            // by attribute name
-            cy.get('[placeholder]')
+        // by attribute name and value
+        cy.get('[placeholder="Email"]')
 
-            // by attribute name and value
-            cy.get('[placeholder="Email"]')
+        // by class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
 
-            // by class value
-            cy.get('[class="input-full-width size-medium shape-rectangle"]')
+        // by tag name, attribute with value
+        cy.get('input[placeholder="Email"]')
 
-            // by tag name, attribute with value
-            cy.get('input[placeholder="Email"]')
+        // by two different attributes
+        cy.get('[placeholder="Email"][type="email"]')
 
-            // by two different attributes
-            cy.get('[placeholder="Email"][type="email"]')
+        // by tag name, attribute with value, id and class name
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
 
-            // by tag name, attribute with value, id and class name
-            cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+        // the most recommended way by cypress (create own locators)
+        cy.get('[data-cy="imputEmail1"]')
+    })
 
-            // the most recommended way by cypress (create own locators)
-            cy.get('[data-cy="imputEmail1"]')
+    it('second test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.get('[data-cy="signInButton"]')
+
+        cy.contains('Sign in')
+
+        cy.contains('[status="warning"]', 'Sign in')
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+
+        cy.contains('nb-card', 'Horizontal form').find('[type="Email"]')
+
+    })
+    it.only('then and wrap methods', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then(secondForm => {
+                const emailLabelSecond = secondForm.find('[for="exampleInputEmail1"]').text()
+                const passwordLabelSecond = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(emailLabelSecond).to.equal('Email address')
+                expect(passwordLabelSecond).to.equal('Password')
+                expect(passwordLabelFirst).to.equal(passwordLabelSecond)
+            })
         })
-        // it('second test', () => {
-        // })
-        // it('third test', () => {
-        // })
+    })
 })
