34a88be700e28d5b322cdffaaf402521d274ed97
Yuriy <Seaman_on_watch@mail.ru>
2020-05-05 12:55:56 +0300

    Checkboxes and radio buttons

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -123,7 +123,7 @@ describe('Our first suite', () => {
             })
     })
 
-    it.only('Invoke command', () => {
+    it('Assert property', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Datepicker').click()
@@ -133,5 +133,28 @@ describe('Our first suite', () => {
             cy.get('nb-calendar-day-picker').contains('17').click()
             cy.wrap(input).invoke('prop', 'value').should('contain', 'May 17, 2020')
         })
+    })
+
+    it('Radio button', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons => {
+            cy.wrap(radioButtons).first().check({ force: true }).should('be.checked')
+            cy.wrap(radioButtons).eq(1).check({ force: true })
+            cy.wrap(radioButtons).first().should('not.be.checked')
+            cy.wrap(radioButtons).eq(2).should('be.disabled')
+        })
+    })
+
+    it.only('Check boxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        // cy.get('[type="checkbox"]').check({ force:true })
+        cy.get('[type="checkbox"]').eq(0).click({force:true})
+        cy.get('[type="checkbox"]').eq(0).check({ force:true })
     })
 })
