a57fb555b5a9daa7a8ec41911ac67f42dcfcbf09
Yuriy <Seaman_on_watch@mail.ru>
2020-05-09 16:05:56 +0300

    Web datepickers

---
+++ cypress/integration/datePicker.spec.js
@@ -0,0 +1,36 @@
+/// <reference types = "cypress" />
+
+describe('Assert property', () => {
+
+    it('Date picker', () => {
+        function selectDayFromCurrent(day) {
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            // console.log(futureDay)
+            let futureMonth = date.toLocaleDateString('en-US', { month: 'short' })
+            // console.log(futureMonth)
+            let dateAssert = futureMonth + ' ' + futureDay + ',' + ' ' + date.getFullYear()
+
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+                if (!dateAttribute.includes(futureMonth)) {
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDayFromCurrent(day)
+                } else {
+                    cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+                }
+            })
+            return dateAssert
+        }
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+            cy.wrap(input).click()
+            let dateAssert = selectDayFromCurrent(5)
+
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
+        })
+    })
+})
